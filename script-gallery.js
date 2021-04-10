c2bfa3961cda96bea22529a4e10fcc0df9001f43
IiIymik <71165032+IiIymik@users.noreply.github.com>
2021-04-10 14:32:50 +0300

    find EL arrow

--- script-gallery.js
+++ script-gallery.js
@@ -1,14 +1,10 @@
 import arrayItems from './gallery-items.js';
-// console.log(arrayItems);
-const boxEL = document.querySelector('.js-gallery');
-// console.log(boxEL);
+
+const galleryImgEl = document.querySelector('.js-gallery');
 const openModal = document.querySelector('.js-lightbox');
-// console.log(openModal);
 const btnCloseEl = document.querySelector('[data-action="close-lightbox"]');
-// console.log(btnCloseEl);
 const openImgEl = document.querySelector('.lightbox__image');
 const overlayEl = document.querySelector('.lightbox__overlay');
-// console.log(overlayEl);
 
 const makeGalleryCard = ({ preview, original, description }) => {
     const liEL = document.createElement('li');
@@ -34,9 +30,9 @@ const makeGalleryCard = ({ preview, original, description }) => {
 
 const elements = arrayItems.map(makeGalleryCard);
 
-boxEL.append(...elements);
+galleryImgEl.append(...elements);
 
-const ulListenerClick = boxEL.addEventListener('click', openGalleryImg);
+galleryImgEl.addEventListener('click', openGalleryImg);
 
 function openGalleryImg(e) {
     e.preventDefault();
@@ -63,24 +59,23 @@ function onEscKeyPress(e) {
 };
 
 function eventArrow(e) {
-    // console.dir(e.code)
-    // console.log(e.target);
-    if (e.code !== 'ArrowLeft') {
-        return
-    };
     const elntChangeing = e.target.children[0].dataset.source
-    // console.log(e.target.children[0].dataset.source)
-    boxEL.childNodes.forEach((el, index) => {
-        let currentelIndex = el;
-        if (e.target.children[0].dataset.source === el.children[0].href) {
-            console.log(el.children[0].href);
-            currentelIndex -= el;
-            // elntChangeing = 
-            console.log(currentelIndex);
-            // console.log(arr);
-            // console.log(arr.indexOf(index));
+    if (e.code === 'ArrowLeft') {
+    galleryImgEl.childNodes.forEach((el) => {
+        if (elntChangeing === el.firstChild.href) {
+            const previousEl = el.previousElementSibling.firstChild.href;
+            openImgEl.src = previousEl;
+            e.repeat;
         }
     })
-    // console.dir(boxEL)
-
+    } else if (e.code === 'ArrowRight') {
+        galleryImgEl.childNodes.forEach((el) => {
+        if (elntChangeing === el.firstChild.href) {
+            const previousEl = el.nextElementSibling.firstChild.href;
+            openImgEl.src = previousEl;
+        }
+    })
+    }
+   
 };
+
